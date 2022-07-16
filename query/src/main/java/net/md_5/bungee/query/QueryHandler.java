2c912ecf6a739e125114ccdf432167702d745c82
2HG <admin@2hg.pw>
2022-07-16 10:52:00 +0700

    I used too much drugs for today

--- query/src/main/java/net/md_5/bungee/query/QueryHandler.java
+++ query/src/main/java/net/md_5/bungee/query/QueryHandler.java
@@ -7,12 +7,6 @@ import io.netty.channel.AddressedEnvelope;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.DatagramPacket;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.config.ListenerInfo;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.LinkedHashMap;
@@ -20,134 +14,158 @@ import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ListenerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 @RequiredArgsConstructor
-public class QueryHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+public class QueryHandler extends SimpleChannelInboundHandler<DatagramPacket>
+{
 
     private final ProxyServer bungee;
     private final ListenerInfo listener;
     /*========================================================================*/
     private final Random random = new Random();
-    private final Cache<InetAddress, QuerySession> sessions = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).build();
+    private final Cache<InetAddress, QuerySession> sessions = CacheBuilder.newBuilder().expireAfterWrite( 30, TimeUnit.SECONDS ).build();
 
-    private void writeShort(ByteBuf buf, int s) {
-        buf.writeShortLE(s);
+    private void writeShort(ByteBuf buf, int s)
+    {
+        buf.writeShortLE( s );
     }
 
-    private void writeNumber(ByteBuf buf, int i) {
-        writeString(buf, Integer.toString(i));
+    private void writeNumber(ByteBuf buf, int i)
+    {
+        writeString( buf, Integer.toString( i ) );
     }
 
-    private void writeString(ByteBuf buf, String s) {
-        for (char c : s.toCharArray()) {
-            buf.writeByte(c);
+    private void writeString(ByteBuf buf, String s)
+    {
+        for ( char c : s.toCharArray() )
+        {
+            buf.writeByte( c );
         }
-        buf.writeByte(0x00);
+        buf.writeByte( 0x00 );
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
-        try {
-            handleMessage(ctx, msg);
-        } catch (Throwable t) {
-            bungee.getLogger().log(Level.WARNING, "Error whilst handling query packet from " + msg.sender(), t);
+    protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception
+    {
+        try
+        {
+            handleMessage( ctx, msg );
+        } catch ( Throwable t )
+        {
+            bungee.getLogger().log( Level.WARNING, "Error whilst handling query packet from " + msg.sender(), t );
         }
     }
 
-    private void handleMessage(ChannelHandlerContext ctx, DatagramPacket msg) {
+    private void handleMessage(ChannelHandlerContext ctx, DatagramPacket msg)
+    {
         ByteBuf in = msg.content();
-        if (in.readUnsignedByte() != 0xFE || in.readUnsignedByte() != 0xFD) {
-            bungee.getLogger().log(Level.WARNING, "Query - Incorrect magic!: {0}", msg.sender());
+        if ( in.readUnsignedByte() != 0xFE || in.readUnsignedByte() != 0xFD )
+        {
+            bungee.getLogger().log( Level.WARNING, "Query - Incorrect magic!: {0}", msg.sender() );
+            ctx.close();
             in.clear();
             return;
         }
 
         ByteBuf out = ctx.alloc().buffer();
-        AddressedEnvelope<ByteBuf, InetSocketAddress> response = new DatagramPacket(out, msg.sender());
+        AddressedEnvelope<ByteBuf, InetSocketAddress> response = new DatagramPacket( out, msg.sender() );
 
         byte type = in.readByte();
         int sessionId = in.readInt();
 
-        if (type == 0x09) {
-            out.writeByte(0x09);
-            out.writeInt(sessionId);
+        if ( type == 0x09 )
+        {
+            out.writeByte( 0x09 );
+            out.writeInt( sessionId );
 
             int challengeToken = random.nextInt();
-            sessions.put(msg.sender().getAddress(), new QuerySession(challengeToken, System.currentTimeMillis()));
+            sessions.put( msg.sender().getAddress(), new QuerySession( challengeToken, System.currentTimeMillis() ) );
 
-            writeNumber(out, challengeToken);
+            writeNumber( out, challengeToken );
         }
 
-        if (type == 0x00) {
+        if ( type == 0x00 )
+        {
             int challengeToken = in.readInt();
-            QuerySession session = sessions.getIfPresent(msg.sender().getAddress());
-            if (session == null || session.getToken() != challengeToken) {
-                throw new IllegalStateException("No session!");
+            QuerySession session = sessions.getIfPresent( msg.sender().getAddress() );
+            if ( session == null || session.getToken() != challengeToken )
+            {
+                throw new IllegalStateException( "No session!" );
             }
 
-            out.writeByte(0x00);
-            out.writeInt(sessionId);
+            out.writeByte( 0x00 );
+            out.writeInt( sessionId );
 
-            switch (in.readableBytes()) {
+            switch ( in.readableBytes() )
+            {
                 case 0:
                     // Short response
-                    writeString(out, listener.getMotd()); // MOTD
-                    writeString(out, "SMP"); // Game Type
-                    writeString(out, "ZCord"); // World Name //ZCord
-                    writeNumber(out, bungee.getOnlineCountBF(true)); // Online Count//ZCord
-                    writeNumber(out, listener.getMaxPlayers()); // Max Players
-                    writeShort(out, listener.getHost().getPort()); // Port
-                    writeString(out, listener.getHost().getHostString()); // IP
+                    writeString( out, listener.getMotd() ); // MOTD
+                    writeString( out, "SMP" ); // Game Type
+                    writeString( out, "ZCord" ); // World Name //ZCord
+                    writeNumber( out, bungee.getOnlineCountBF( true ) ); // Online Count//ZCord
+                    writeNumber( out, listener.getMaxPlayers() ); // Max Players
+                    writeShort( out, listener.getHost().getPort() ); // Port
+                    writeString( out, listener.getHost().getHostString() ); // IP
                     break;
                 case 4:
                     // Long Response
-                    out.writeBytes(new byte[]
-                            {
-                                    0x73, 0x70, 0x6C, 0x69, 0x74, 0x6E, 0x75, 0x6D, 0x00, (byte) 0x80, 0x00
-                            });
+                    out.writeBytes( new byte[]
+                    {
+                        0x73, 0x70, 0x6C, 0x69, 0x74, 0x6E, 0x75, 0x6D, 0x00, (byte) 0x80, 0x00
+                    } );
                     Map<String, String> data = new LinkedHashMap<>();
-                    data.put("hostname", listener.getMotd());
-                    data.put("gametype", "SMP");
+                    data.put( "hostname", listener.getMotd() );
+                    data.put( "gametype", "SMP" );
                     // Start Extra Info
-                    data.put("game_id", "MINECRAFT");
-                    data.put("version", bungee.getCustomBungeeName()); //ZCord
-                    data.put("plugins", "");
+                    data.put( "game_id", "MINECRAFT" );
+                    data.put( "version", bungee.getCustomBungeeName() ); //ZCord
+                    data.put( "plugins", "" );
                     // End Extra Info
-                    data.put("map", "ZCord by HungNguyenAZ"); //ZCord
-                    data.put("numplayers", Integer.toString(bungee.getOnlineCountBF(true))); //ZCord
-                    data.put("maxplayers", Integer.toString(listener.getMaxPlayers()));
-                    data.put("hostport", Integer.toString(listener.getHost().getPort()));
-                    data.put("hostip", listener.getHost().getHostString());
-                    for (Map.Entry<String, String> entry : data.entrySet()) {
-                        writeString(out, entry.getKey());
-                        writeString(out, entry.getValue());
+                    data.put( "map", "ZCord by HungNguyenAZ" ); //ZCord
+                    data.put( "numplayers", Integer.toString( bungee.getOnlineCountBF( true ) ) ); //ZCord
+                    data.put( "maxplayers", Integer.toString( listener.getMaxPlayers() ) );
+                    data.put( "hostport", Integer.toString( listener.getHost().getPort() ) );
+                    data.put( "hostip", listener.getHost().getHostString() );
+                    for ( Map.Entry<String, String> entry : data.entrySet() )
+                    {
+                        writeString( out, entry.getKey() );
+                        writeString( out, entry.getValue() );
                     }
-                    out.writeByte(0x00); // Null
+                    out.writeByte( 0x00 ); // Null
                     // Padding
-                    writeString(out, "\01player_\00");
+                    writeString( out, "\01player_\00" );
                     // Player List
-                    for (ProxiedPlayer p : bungee.getPlayers()) {
-                        writeString(out, p.getName());
+                    for ( ProxiedPlayer p : bungee.getPlayers() )
+                    {
+                        writeString( out, p.getName() );
                     }
-                    out.writeByte(0x00); // Null
+                    out.writeByte( 0x00 ); // Null
                     break;
                 default:
-                    ctx.close();
-                    throw new IllegalStateException("Invalid data request packet");
+                    // Error!
+                    throw new IllegalStateException( "Invalid data request packet" );
             }
         }
 
-        ctx.writeAndFlush(response);
+        ctx.writeAndFlush( response );
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    {
         ctx.close(); //ZCord closes connection if query packet throwed an exception before login
-        bungee.getLogger().log(Level.WARNING, "Error whilst handling query packet from " + ctx.channel().remoteAddress(), cause);
+        bungee.getLogger().log( Level.WARNING, "Error whilst handling query packet from " + ctx.channel().remoteAddress(), cause );
     }
 
     @Data
-    private static class QuerySession {
+    private static class QuerySession
+    {
 
         private final int token;
         private final long time;
