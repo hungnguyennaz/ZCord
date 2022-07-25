6f85581fb859018794834d047fb0532a275549b2
HungNguyenAZ <admin@2hg.pw>
2022-07-25 11:10:05 +0700

    Only java 17 and up are supported

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -4,9 +4,9 @@ public class Bootstrap
 {
 
     public static void main(String[] args) throws Exception {
-        if (Float.parseFloat(System.getProperty("java.class.version") ) < 52.0) {  // ZCord
+        if (Float.parseFloat(System.getProperty("java.class.version") ) < 60.0) {  // ZCord
             System.err.println("Your Java version (" + System.getProperty("java.version") + ") is too old. Please update your Java runtime."); // ZCord
-            System.out.println("Java 8 or later is required to run ZCord."); // ZCord
+            System.out.println("Java 17 or later is required to run ZCord."); // ZCord
             return;
         }
 
