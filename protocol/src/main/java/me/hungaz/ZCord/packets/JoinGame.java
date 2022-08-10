314bf73d083ff2be41111b6f67be7d8b393b9e01
KurumiFake <kurumiisshidowife@gmail.com>
2022-08-10 11:33:51 +0000

    Revert "feat -> Bot-Filter 1.19/1.19.1 support by xIsm4"
    This reverts commit 257735f70b55521597c38a2667b3f717ce79731b.
    
    hungnguyennaz told me to so idk

--- protocol/src/main/java/me/hungaz/ZCord/packets/JoinGame.java
+++ protocol/src/main/java/me/hungaz/ZCord/packets/JoinGame.java
@@ -1,28 +1,28 @@
 package me.hungaz.ZCord.packets;
 
 import io.netty.buffer.ByteBuf;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
-import me.hungaz.ZCord.utils.Dimension;
-import net.md_5.bungee.protocol.AbstractPacketHandler;
-import net.md_5.bungee.protocol.DefinedPacket;
-import net.md_5.bungee.protocol.ProtocolConstants;
-import se.llbit.nbt.Tag;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.protocol.AbstractPacketHandler;
+import net.md_5.bungee.protocol.DefinedPacket;
+import net.md_5.bungee.protocol.ProtocolConstants;
+import me.hungaz.ZCord.utils.Dimension;
+import se.llbit.nbt.Tag;
 
 @Data
 @RequiredArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-public class JoinGame extends DefinedPacket {
+public class JoinGame extends DefinedPacket
+{
     private final int entityId;
     private boolean hardcore = false;
     private short gameMode = 0;
     private short previousGameMode = 0;
-    private Set<String> worldNames = new HashSet<>(Arrays.asList("minecraft:overworld"));
+    private Set<String> worldNames = new HashSet<>( Arrays.asList( "minecraft:overworld" ) );
     private String worldName = "minecraft:overworld";
     private int dimensionId = 0;
     private long seed = 1;
@@ -35,120 +35,143 @@ public class JoinGame extends DefinedPacket {
     private boolean debug = false;
     private boolean flat = true;
 
-    private Tag dimensions1_16;
-    private Tag dimensions1_16_2;
-    private Tag dimensions1_18_2;
-    private Tag dimensions1_19;
-    private Tag dimensions1_19_1;
+    private Tag dimensions116;
+    private Tag dimensions1162;
+    private Tag dimensions1182;
+    private Tag dimensions119;
 
     private Tag dimension;
-    private Tag dimension1_18_2;
-
-    public JoinGame() {
-        this(0, Dimension.OVERWORLD);
+    private Tag dimension1182;
+    public JoinGame()
+    {
+        this ( 0, Dimension.OVERWORLD );
     }
-
-    public JoinGame(int entityId, Dimension dimension) {
+    public JoinGame(int entityId, Dimension dimension)
+    {
         this.entityId = entityId;
         this.dimensionId = dimension.getDimensionId();
         this.worldName = dimension.getKey();
-        this.worldNames = new HashSet<>(Arrays.asList(dimension.getKey()));
+        this.worldNames = new HashSet<>( Arrays.asList( dimension.getKey() ) );
 
-        this.dimensions1_16 = dimension.getFullCodec(ProtocolConstants.MINECRAFT_1_16_1);
-        this.dimensions1_16_2 = dimension.getFullCodec(ProtocolConstants.MINECRAFT_1_16_2);
-        this.dimensions1_18_2 = dimension.getFullCodec(ProtocolConstants.MINECRAFT_1_18_2);
-        this.dimensions1_19 = dimension.getFullCodec(ProtocolConstants.MINECRAFT_1_19);
-        this.dimensions1_19_1 = dimension.getFullCodec(ProtocolConstants.MINECRAFT_1_19_1);
+        this.dimensions116 = dimension.getFullCodec( ProtocolConstants.MINECRAFT_1_16_1 );
+        this.dimensions1162 = dimension.getFullCodec( ProtocolConstants.MINECRAFT_1_16_2 );
+        this.dimensions1182 = dimension.getFullCodec( ProtocolConstants.MINECRAFT_1_18_2 );
+        this.dimensions119 = dimension.getFullCodec( ProtocolConstants.MINECRAFT_1_19 );
 
-        this.dimension = dimension.getAttributes(ProtocolConstants.MINECRAFT_1_16_2);
-        this.dimension1_18_2 = dimension.getAttributes(ProtocolConstants.MINECRAFT_1_18_2);
+        this.dimension = dimension.getAttributes( ProtocolConstants.MINECRAFT_1_16_2 );
+        this.dimension1182 = dimension.getAttributes( ProtocolConstants.MINECRAFT_1_18_2 );
     }
 
     @Override
-    public void read(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion) {
+    public void read(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion)
+    {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void write(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion) {
-        buf.writeInt(entityId);
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_16_2) {
-            buf.writeBoolean(hardcore);
+    public void write(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion)
+    {
+        buf.writeInt( entityId );
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_16_2 )
+        {
+            buf.writeBoolean( hardcore );
         }
-        buf.writeByte(gameMode);
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_16) {
-            buf.writeByte(previousGameMode);
+        buf.writeByte( gameMode );
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_16 )
+        {
+            buf.writeByte( previousGameMode );
 
-            writeVarInt(worldNames.size(), buf);
-            for (String world : worldNames) {
-                writeString(world, buf);
+            writeVarInt( worldNames.size(), buf );
+            for ( String world : worldNames )
+            {
+                writeString( world, buf );
             }
 
-            if (protocolVersion >= ProtocolConstants.MINECRAFT_1_19_1) {
-                writeTag(dimensions1_19_1, buf);
-            } else if (protocolVersion >= ProtocolConstants.MINECRAFT_1_19) {
-                writeTag(dimensions1_19, buf);
-            } else if (protocolVersion >= ProtocolConstants.MINECRAFT_1_18_2) {
-                writeTag(dimensions1_18_2, buf);
-            } else if (protocolVersion >= ProtocolConstants.MINECRAFT_1_16_2) {
-                writeTag(dimensions1_16_2, buf);
-            } else {
-                writeTag(dimensions1_16, buf);
+            if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_19 )
+            {
+                writeTag( dimensions119, buf );
+            } else if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_18_2 )
+            {
+                writeTag( dimensions1182, buf );
+            } else if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_16_2 )
+            {
+                writeTag( dimensions1162, buf );
+            } else
+            {
+                writeTag( dimensions116, buf );
             }
         }
 
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_16) {
-            if (protocolVersion >= ProtocolConstants.MINECRAFT_1_19 || protocolVersion <= ProtocolConstants.MINECRAFT_1_16_1) {
-                writeString(worldName, buf);
-            } else if (protocolVersion == ProtocolConstants.MINECRAFT_1_18_2) {
-                writeTag(dimension1_18_2, buf);
-            } else {
-                writeTag(dimension, buf);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_16 )
+        {
+            if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_19 || protocolVersion <= ProtocolConstants.MINECRAFT_1_16_1 )
+            {
+                writeString( worldName, buf );
+            } else if ( protocolVersion == ProtocolConstants.MINECRAFT_1_18_2 )
+            {
+                writeTag( dimension1182, buf );
+            } else
+            {
+                writeTag( dimension, buf );
             }
-            writeString(worldName, buf);
-        } else if (protocolVersion > ProtocolConstants.MINECRAFT_1_9) {
-            buf.writeInt(dimensionId); //dim
-        } else {
-            buf.writeByte(dimensionId); //dim
+            writeString( worldName, buf );
+        } else if ( protocolVersion > ProtocolConstants.MINECRAFT_1_9 )
+        {
+            buf.writeInt( dimensionId ); //dim
+        } else
+        {
+            buf.writeByte( dimensionId ); //dim
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_15) {
-            buf.writeLong(seed);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_15 )
+        {
+            buf.writeLong( seed );
         }
-        if (protocolVersion < ProtocolConstants.MINECRAFT_1_14) {
-            buf.writeByte(difficulty);
+        if ( protocolVersion < ProtocolConstants.MINECRAFT_1_14 )
+        {
+            buf.writeByte( difficulty );
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_16_2) {
-            writeVarInt(maxPlayers, buf);
-        } else {
-            buf.writeByte(maxPlayers);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_16_2 )
+        {
+            writeVarInt( maxPlayers, buf );
+        } else
+        {
+            buf.writeByte( maxPlayers );
         }
-        if (protocolVersion < ProtocolConstants.MINECRAFT_1_16) {
-            writeString(levelType, buf);
+        if ( protocolVersion < ProtocolConstants.MINECRAFT_1_16 )
+        {
+            writeString( levelType, buf );
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_14) {
-            writeVarInt(viewDistance, buf);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_14 )
+        {
+            writeVarInt( viewDistance, buf );
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_18) {
-            writeVarInt(viewDistance, buf);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_18 )
+        {
+            writeVarInt( viewDistance, buf );
         }
-        if (protocolVersion >= 29) {
-            buf.writeBoolean(reducedDebugInfo);
+        if ( protocolVersion >= 29 )
+        {
+            buf.writeBoolean( reducedDebugInfo );
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_15) {
-            buf.writeBoolean(normalRespawn);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_15 )
+        {
+            buf.writeBoolean( normalRespawn );
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_16) {
-            buf.writeBoolean(debug);
-            buf.writeBoolean(flat);
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_16 )
+        {
+            buf.writeBoolean( debug );
+            buf.writeBoolean( flat );
         }
-        if (protocolVersion >= ProtocolConstants.MINECRAFT_1_19) {
-            buf.writeBoolean(false); //lastDeathPos
+        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_19 )
+        {
+            buf.writeBoolean( false ); //lastDeathPos
         }
 
     }
 
     @Override
-    public void handle(AbstractPacketHandler handler) throws Exception {
+    public void handle(AbstractPacketHandler handler) throws Exception
+    {
         throw new UnsupportedOperationException();
     }
 }
